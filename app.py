74b6ecdbc935f2e9773d720e91c9bc05f29bdd3b
sakhilpratap620 <you@example.com>
2022-06-28 08:42:07 +0530

    ist change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/", methods= ['GET','POST'])
 def index():
-    return "starting machine learning code"
+    return "starting machine learning code first update"
 
 if __name__=="__main__":
     app.run(debug=True)    
