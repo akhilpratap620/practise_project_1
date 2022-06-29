3448be6cfe5778836f982c2801a57a7a39cc8175
sakhilpratap620 <you@example.com>
2022-06-29 10:08:36 +0530

    latest

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup
+from setuptools import setup , find_packages
 from typing import List
 
 
 #declaring variable for setup function
+
 PROJECT_NAME="housing-predict"
-VERSION="0.0.1" 
+VERSION="0.0.2" 
 AUTHOR="Akhil pratap singh"
 DSCRIPTION="this is first fsds batch project"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 """txt
 this function is going to return requirment.return 
 list of librraries required for package
@@ -26,7 +28,7 @@ setup(
     version =VERSION, 
     author=AUTHOR,
     discription=DSCRIPTION,
-    packages=["housing"] ,
+    packages=find_packages() ,
     install_requires=get_requirements_list()
 
 )
