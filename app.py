7497885b5b29de57cf7120a7b8ecaac34bd61ace
sakhilpratap620 <you@example.com>
2022-07-01 08:29:48 +0530

    module update

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app = Flask(__name__)
 
 @app.route("/", methods= ['GET','POST'])
 def index():
+    try:
+        raise Exception("we are testing exception class")
+    except Exception as e:
+        housing = HousingException(e, sys)   
+        logging.info(housing.error_message) 
+        logging.info("we are testing logging module")
     return "starting machine learning code first update"
 
 if __name__=="__main__":
